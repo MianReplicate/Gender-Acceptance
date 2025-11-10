d49e97f9dd5bbe37ccb8a8098a989db1c4a281a4
Mian <docgoosenn@gmail.com>
2025-11-10 10:41:14 -0800

    Finished version tracker Now for bug testing!

---
+++ Source/GenderAcceptance/Mian/Utilities/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace GenderAcceptance.Mian;
+
+public class Constants
+{
+    public const string Version = "1.1";
+
+    public static readonly List<string> WarnOnVersion = new()
+    {
+        "1.1"
+    };
+}
